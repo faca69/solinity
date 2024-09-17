cf1669ecb2730153a47c4bd4301543302d000188
faca69 <fnikolov123456789@gmail.com>
2024-09-17 20:57:58 +0200

    advertise page setup

--- client/src/app/advertise/page.tsx
+++ client/src/app/advertise/page.tsx
@@ -1,8 +1,23 @@
 export default function AdvertisePage() {
   return (
-    <div>
+    <div className="flex flex-col text-center ">
       <div className="bg-emerald-700/80 h-[400px] w-[400px] blur-[400px] absolute top-[200px] left-[750px] -z-10 "></div>
-      <h1 className="text-6xl font-bold py-11 text-center ">Advertise</h1>
+      <h1 className="text-6xl font-bold py-11 text-center bg-gradient-to-b from-emerald-300 to-emerald-700 bg-clip-text text-transparent">
+        Boost Your Presales's Visibility!
+      </h1>
+
+      <h2 className="text-4xl font-semibold bg-gradient-to-b from-gray-100 to-gray-300 bg-clip-text text-transparent">
+        Get your presale featured at the top of the list.
+      </h2>
+
+      <div>
+        <div>
+          <p>$299</p>
+          <p>USD Equivelant</p>
+
+          <p></p>
+        </div>
+      </div>
     </div>
   );
 }
