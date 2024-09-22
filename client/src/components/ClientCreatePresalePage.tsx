4099fd3802c2f0bb1c646b5603166bb88453ffb7
faca69 <fnikolov123456789@gmail.com>
2024-09-22 16:37:29 +0200

    form watch 2

--- client/src/components/ClientCreatePresalePage.tsx
+++ client/src/components/ClientCreatePresalePage.tsx
@@ -428,16 +428,14 @@ export default function ClientCreatePresalePage() {
             />
           </div>
 
-          {[
-            "isAdvertised",
-            "name",
-            "symbol",
-            "image",
-            "description",
-            "totalSupply",
-            "useOfFunds",
-            "solanaAddress",
-          ].every((field) => form.watch(field)) ? (
+          {form.watch("isAdvertised") &&
+          form.watch("name") &&
+          form.watch("symbol") &&
+          form.watch("image") &&
+          form.watch("description") &&
+          form.watch("totalSupply") &&
+          form.watch("useOfFunds") &&
+          form.watch("solanaAddress") ? (
             <button
               className="p-[3px] relative"
               type="submit"
